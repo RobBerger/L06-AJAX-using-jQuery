4d3b6438a78950c059ec1ee8770826b3a1462ecb
berger <robertberger.dev@gmail.com>
2022-12-31 11:09:01 -0500

    final example code

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-$.ajax('http://www.boredapi.com/api/activity',
+$.ajax(
+    'http://www.boredapi.com/api/activity',
     {
         success: function (APIResponse) {
-            console.log(APIResponse);
+            // Creating a div element
+            let myDiv = document.createElement('div');
+
+            // Creating a paragraph element and adding the innerHTML
+            let activityTag = document.createElement('p');
+            activityTag.innerHTML = "Activity Name: " + APIResponse.activity;
+
+            // Creating another paragraph element and adding the innerHTML
+            let participantsTag = document.createElement('p');
+            participantsTag.innerHTML = "Number of Participants: " + APIResponse.participants;
+
+            // Appending the paragraph tags to the created div element
+            myDiv.appendChild(activityTag);
+            myDiv.appendChild(participantsTag);
+
+            document.body.appendChild(myDiv);
         }
     })
